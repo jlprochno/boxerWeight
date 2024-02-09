8c9d3544caaa67d3eb0dfd2db0ca11ea5bf6c4be
Jenyffer Prochno <jlprochno@gmail.com>
2024-02-09 09:51:08 -0300

    Publishing

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace boxerWeight;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Olá, por gentileza digite o peso do boxeador: ");
+        double weight = Convert.ToDouble(Console.ReadLine());
+
+        if(weight <= 50){
+            System.Console.WriteLine("Peso Palha");
+        }
+        else if(weight < 60){
+            System.Console.WriteLine("Peso Pluma");
+        }
+        else if(weight < 76){
+            Console.WriteLine("Peso Leve");
+        }
+        else if(weight < 88){
+           Console.WriteLine("Peso Pesado");
+        }
+        else{
+            Console.WriteLine("Peso Super Pesado");
+        }
+    }
+}
+
+
+
+
+    
